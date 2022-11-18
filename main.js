98bc79b4a0f1a37fe42b95c53ea54322ad7bd4d6
Marcos Wright-Kuhns <mwrightkuhns@pagerduty.com>
2022-11-17 21:53:34 -0800

    Render a tree of stories

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import fullStory from "./story.js";
+import { storyLayers, allStories } from "./story.js";
 
-const currentStorySteps = [1,2];
+const currentStorySteps = [1, 2];
 
 if ("content" in document.createElement("template")) {
   console.log("A supported browser!");
@@ -9,26 +9,56 @@ if ("content" in document.createElement("template")) {
 }
 
 const renderStory = () => {
-  const template = document.getElementById("story_template");
-  currentStorySteps.forEach((index) => {
-    const { name, children } = fullStory[index];
-    const storyEl = template.content.cloneNode(true);
+  const main = document.querySelector("main");
+
+  storyLayers.forEach((stories, index) => {
+    renderRow({ stories, parentEl: main, index });
+  });
+};
+
+const renderRow = ({ stories, parentEl, index }) => {
+  const rowEl = document.createElement("div");
+  rowEl.setAttribute("id", `row-${index}`);
+  rowEl.classList.add("row");
+  stories.forEach((story) => {
+    renderCell({ story, parentEl: rowEl });
+  });
+
+  parentEl.append(rowEl);
+};
+
+const renderCell = ({ story, parentEl }) => {
+  const storyEl = document.createElement("div");
+  storyEl.classList.add("story");
+
+  if(Object.keys(story).length) {
+    const cellTemp = document.getElementById("cell_template");
+    const { name, children } = story;
+    storyEl.append(cellTemp.content.cloneNode(true));
 
     storyEl.querySelector("h2").textContent = name;
 
     const childrenEl = storyEl.querySelector(".children");
-    children.forEach((childIndex) => renderChild(childIndex, childrenEl));
+    if (children.length) {
+      children.forEach((childId) =>
+        renderChild({ childId, parentEl: childrenEl.querySelector("ol") })
+      );
+    } else {
+      childrenEl.classList.add("end");
+      childrenEl.textContent = "The End";
+    }
+  }
 
-    document.querySelector("main").append(storyEl);
-  });
+
+  parentEl.append(storyEl);
 };
 
-const renderChild = (childIndex, parentEl) => {
+const renderChild = ({ childId, parentEl }) => {
   const childEl = document.createElement("li");
-  childEl.setAttribute("value", childIndex);
-  childEl.textContent = fullStory[childIndex].name;
+  childEl.setAttribute("value", childId);
+  childEl.textContent = allStories[childId].name;
   parentEl.append(childEl);
-}
+};
 
 document.addEventListener("DOMContentLoaded", () => {
   renderStory();
--- story.js
+++ story.js
@@ -1,31 +1,49 @@
-const story = {
-  1: { name: "Welcome to Vanguard Estates", children: [2, 3] },
-  2: { name: "Stay at Vanguard", children: [6, 7] },
-  3: { name: "Bring Him Home", children: [4, 5] },
-  4: { name: "Buy A Home Robot", children: [8, 9] },
-  5: { name: "Call Him More", children: [10, 11] },
-  6: { name: "Use Drop-In", children: [12, 13] },
-  7: { name: "Disable Drop-In", children: [14, 15] },
-  8: { name: "Explain Missy", children: [16, 17] },
-  9: { name: "Don't Explain Missy", children: [] },
-  10: { name: "Install Cameras", children: [18, 19] },
-  11: { name: "ove Him In", children: [20, 21] },
-  12: { name: "Don't Tell Him", children: [22, 23] },
-  13: { name: "Tell Him", children: [24, 25] },
-  14: { name: "Robot Baths", children: [26, 27] },
-  15: { name: "Human Baths", children: [] },
-  16: { name: "Go To Manila", children: [] },
-  17: { name: "Don't Go To Manila", children: [] },
-  18: { name: "Handle it Yourself", children: [] },
-  19: { name: "Pay for Help", children: [] },
-  20: { name: "Drop Out of School", children: [] },
-  21: { name: "Take Out a Loan", children: [] },
-  22: { name: "Stop the Robot", children: [] },
-  23: { name: "Trust the Robot", children: [] },
-  24: { name: "Keep His Secret", children: [] },
-  25: { name: "Report Him", children: [] },
-  26: { name: "Break the News", children: [] },
-  27: { name: "Keep Quiet", children: [] },
-};
+export const storyLayers = [
+  [{ id: "1", name: "Welcome to Vanguard Estates", children: [2, 3] }],
+  [
+    { id: "2", name: "Stay at Vanguard", children: [6, 7] },
+    { id: "3", name: "Bring Him Home", children: [4, 5] },
+  ],
+  [
+    { id: "6", name: "Use Drop-In", children: [12, 13] },
+    { id: "7", name: "Disable Drop-In", children: [14, 15] },
+    { id: "4", name: "Buy A Home Robot", children: [8, 9] },
+    { id: "5", name: "Call Him More", children: [10, 11] },
+  ],[
+    { id: "12", name: "Don't Tell Him", children: [22, 23] },
+    { id: "13", name: "Tell Him", children: [24, 25] },
+    { id: "14", name: "Robot Baths", children: [26, 27] },
+    { id: "15", name: "Human Baths", children: [] },
+    { id: "8", name: "Explain Missy", children: [16, 17] },
+    { id: "9", name: "Don't Explain Missy", children: [] },
+    { id: "10", name: "Install Cameras", children: [18, 19] },
+    { id: "11", name: "Move Him In", children: [20, 21] },
+  ],
+  [
+    { id: "22", name: "Stop the Robot", children: [] },
+    { id: "23", name: "Trust the Robot", children: [] },
+    { id: "24", name: "Keep His Secret", children: [] },
+    { id: "25", name: "Report Him", children: [] },
+    { id: "26", name: "Break the News", children: [] },
+    { id: "27", name: "Keep Quiet", children: [] },
+    {},
+    {},
+    { id: "16", name: "Go To Manila", children: [] },
+    { id: "17", name: "Don't Go To Manila", children: [] },
+    {},
+    {},
+    { id: "18", name: "Handle it Yourself", children: [] },
+    { id: "19", name: "Pay for Help", children: [] },
+    { id: "20", name: "Drop Out of School", children: [] },
+    { id: "21", name: "Take Out a Loan", children: [] },
+  ]
+];
 
-export default story;
+export const allStories = storyLayers.reduce((acc, stories) => {
+  const newStories = stories.reduce((acc, story) => {
+    acc[story.id] = story;
+    return acc;
+  }, {});
+  Object.assign(acc, newStories)
+  return acc;
+}, {});
